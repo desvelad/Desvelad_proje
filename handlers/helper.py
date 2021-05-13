0a11d5f48855bd1988d4e8625872ef6a007dbe71
EfsaneStar <82456249+Mehmetbaba55@users.noreply.github.com>
2021-05-13 08:19:27 +0300

    Update helper.py

--- handlers/helper.py
+++ handlers/helper.py
@@ -12,18 +12,18 @@ async def helper(ok, message: Message):
         f"""💞 Merhaba! Aşağıdaki komutlar şunlardır: **{bn}** - __A Grup Sesli Sohbet Müzik Çaları__.
 Şu anda desteklediğim komutlar şunlardır:
 
-🔥 **Kullanıcı Komutları :**
-⚜️ /oynat - **[ Yalnızca Gruplar ]** > __Yanıtlanan ses dosyasını veya YouTube videosunu bağlantı üzerinden çalar.__
-⚜️ /bul - **[ Gruplar & İçinde]** > __Sohbette aranan şarkıyı yükler.__
-⚜️ /ytplay - **[ Yalnızca Gruplar]** > __Şarkıyı doğrudan YouTube Arama'dan çalar.__
-⚜️ /düzenleyen - **[ Bilgi ]** > __Düzenleyen kişi bilgisini gösterir.__
+👤 **Kullanıcı Komutları :**
+▶️ /oynat - **[ Yalnızca Gruplar ]** > __Yanıtlanan ses dosyasını veya YouTube videosunu bağlantı üzerinden çalar.__
+🔎 /bul - **[ Gruplar & İçinde]** > __Sohbette aranan şarkıyı yükler.__
+⏩ /ytplay - **[ Yalnızca Gruplar]** > __Şarkıyı doğrudan YouTube Arama'dan çalar.__
+🇹🇷 /düzenleyen - **[ Bilgi ]** > __Düzenleyen kişi bilgisini gösterir.__
 
 
-🔰 **Yönetici ve Özel Kullanıcıları Komutları :**
-⚜️ /durdur - **[Yalnızca Gruplar ]** > __Pause Voice Chat Music.__
-⚜️ /devam - **[Yalnızca Gruplar ]** > __Resume Voice Chat Music.__
-⚜️ /atla - **[Yalnızca Gruplar ]** > __Skips the current Music Playing In Voice Chat.__
-⚜️ /dur - **[Yalnızca Gruplar ]** > __Clears The Queue as well as ends Voice Chat Music.__""")
+👮‍♂️ **Yönetici ve Özel Kullanıcıları Komutları :**
+⏸️ /durdur - **[Yalnızca Gruplar ]** > __Pause Voice Chat Music.__
+⏩ /devam - **[Yalnızca Gruplar ]** > __Resume Voice Chat Music.__
+⏭️ /atla - **[Yalnızca Gruplar ]** > __Skips the current Music Playing In Voice Chat.__
+🛑 /dur - **[Yalnızca Gruplar ]** > __Clears The Queue as well as ends Voice Chat Music.__""")
 
 @Client.on_message(command("help") & other_filters)
 async def ghelp(_, message: Message):
